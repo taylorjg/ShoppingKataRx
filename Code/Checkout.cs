35db61ba2af3e42c75275045c17685d3715b73ef
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-03-23 23:55:30 +0000

    Added the first test

---
+++ Code/Checkout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Code
+{
+    public class Checkout
+    {
+        public Checkout(IObservable<string> scannedItems, Action<int> onItem)
+        {
+            var subscription = scannedItems.Subscribe(
+                item => onItem(50),
+                () => { });
+            //subscription.Dispose();
+        }
+    }
+}
---
+++ Tests/ShoppingKataTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Subjects;
+using Code;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class ShoppingKataTests
+    {
+        [Test]
+        public void SingleItemHasTheCorrectPrice()
+        {
+            // Arrange
+            var total = 0;
+            var scannedItems = new Subject<string>();
+            var checkout = new Checkout(scannedItems, t => total = t);
+
+            // Act
+            scannedItems.OnNext("A");
+            scannedItems.OnCompleted();
+
+            // Assert
+            Assert.That(total, Is.EqualTo(50));
+        }
+    }
+}
