2a26421e907ab0d1b949422f5f00ff72a11a9e41
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-03-24 21:43:57 +0000

    Implemented IObservable<string> over Console.In.ReadLineAsync

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using Code;
 
@@ -9,32 +11,14 @@ namespace App
     {
         private static void Main(string[] args)
         {
-            IObservable<string> sequenceOfItems = null;
-
-            if (args.Length > 0)
+            if (args.Length > 1)
             {
-                if (args.Length > 1)
-                {
-                    Usage();
-                }
+                Usage();
+            }
 
-                var rs = new ReplaySubject<string>();
-                foreach (var item in args[0].Select(c => Convert.ToString(c)))
-                {
-                    rs.OnNext(item);
-                }
-                rs.OnCompleted();
-                sequenceOfItems = rs;
-            }
-            else
-            {
-                // Need to create an observable to read console input
-                // Can we read key presses asynchronously via Console.In.ReadAsync() ?
-                //var buffer = new char[1];
-                //Console.In.ReadAsync(buffer, 0, buffer.Length);
-                // Q,q => OnCompleted()
-                // anythin else => OnNext()
-            }
+            var sequenceOfItems = (args.Length == 1)
+                                      ? CreateSequenceOfItemsOverCommandLineArgument(args)
+                                      : CreateSequenceOfItemsOverAsyncConsoleReadLoop();
 
             var checkout = new Checkout();
             var total = 0;
@@ -43,12 +27,68 @@ namespace App
             Console.WriteLine("Total = {0}.", total);
         }
 
+        private static IObservable<string> CreateSequenceOfItemsOverCommandLineArgument(IList<string> args)
+        {
+            var sequenceOfItems = new ReplaySubject<string>();
+            foreach (var item in args[0].Select(c => Convert.ToString(c)))
+            {
+                sequenceOfItems.OnNext(item);
+            }
+            sequenceOfItems.OnCompleted();
+            return sequenceOfItems;
+        }
+
+        private static IObservable<string> CreateSequenceOfItemsOverAsyncConsoleReadLoop()
+        {
+            return Observable.Create<string>(observer =>
+                {
+                    Log("Calling ConsoleKeyPressReadLoop()...");
+                    ConsoleKeyPressReadLoop(observer);
+                    Log("...returned from ConsoleKeyPressReadLoop()");
+                    return () => Log("Inside the subscription dispose action");
+                });
+        }
+
+        private async static void ConsoleKeyPressReadLoop(IObserver<string> observer)
+        {
+            for (;;)
+            {
+                Log("Calling await Console.In.ReadLineAsync()");
+                var line = await Console.In.ReadLineAsync();
+                Log("...after await Console.In.ReadLineAsync() - line.Length: {0}", line.Length);
+
+                if (line.Length == 0)
+                {
+                    Log("Calling observer.OnCompleted() due to empty line");
+                    observer.OnCompleted();
+                    return;
+                }
+
+                foreach (var keyPress in line)
+                {
+                    if (keyPress == 'Q' || keyPress == 'q')
+                    {
+                        Log("Calling observer.OnCompleted() due to '{0}'", keyPress);
+                        observer.OnCompleted();
+                        return;
+                    }
+
+                    Log("Calling observer.OnNext('{0}')", keyPress);
+                    observer.OnNext(Convert.ToString(keyPress));
+                }
+            }
+        }
+
         private static void Usage()
         {
             Console.Error.WriteLine("App [ <list of items> ]");
-            Console.Error.WriteLine("");
-            Console.Error.WriteLine("\tApp AABC");
+            Console.Error.WriteLine("    e.g. App AABC");
             Environment.Exit(1);
+        }
+
+        private static void Log(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
         }
     }
 }
