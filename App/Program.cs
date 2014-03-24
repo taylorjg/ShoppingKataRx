4ce4a59e9e4cd2d2a0bc984ef46ae0736ef115c6
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-03-24 21:58:29 +0000

    Added more logging to the app

--- App/Program.cs
+++ App/Program.cs
@@ -22,59 +22,75 @@ namespace App
 
             var checkout = new Checkout();
             var total = 0;
-            checkout.ProcessSequenceOfItems(sequenceOfItems, newTotal => total += newTotal);
+            checkout.ProcessSequenceOfItems(sequenceOfItems, totalDelta =>
+                {
+                    Log("totalDelta: {0}", totalDelta);
+                    total += totalDelta;
+                });
             checkout.Reset();
             Console.WriteLine("Total = {0}.", total);
         }
 
         private static IObservable<string> CreateSequenceOfItemsOverCommandLineArgument(IList<string> args)
         {
-            var sequenceOfItems = new ReplaySubject<string>();
-            foreach (var item in args[0].Select(c => Convert.ToString(c)))
+            using (new LogEntryExit("CreateSequenceOfItemsOverCommandLineArgument"))
             {
-                sequenceOfItems.OnNext(item);
+                var sequenceOfItems = new ReplaySubject<string>();
+                foreach (var item in args[0].Select(c => Convert.ToString(c)))
+                {
+                    sequenceOfItems.OnNext(item);
+                }
+                sequenceOfItems.OnCompleted();
+                return sequenceOfItems;
             }
-            sequenceOfItems.OnCompleted();
-            return sequenceOfItems;
         }
 
         private static IObservable<string> CreateSequenceOfItemsOverAsyncConsoleReadLoop()
         {
-            return Observable.Create<string>(observer =>
+            using (new LogEntryExit("CreateSequenceOfItemsOverAsyncConsoleReadLoop"))
+            {
+                return Observable.Create<string>(observer =>
                 {
-                    Log("Calling ConsoleKeyPressReadLoop()...");
-                    ConsoleKeyPressReadLoop(observer);
-                    Log("...returned from ConsoleKeyPressReadLoop()");
-                    return () => Log("Inside the subscription dispose action");
+                    using (new LogEntryExit("Observable.Create()'s subscribe lambda"))
+                    {
+                        Log("Calling ConsoleKeyPressReadLoop()...");
+                        ConsoleKeyPressReadLoop(observer);
+                        Log("...returned from ConsoleKeyPressReadLoop()");
+                        return () => Log("Inside the subscription dispose action");
+                    }
                 });
+            }
         }
 
         private async static void ConsoleKeyPressReadLoop(IObserver<string> observer)
         {
-            for (;;)
+            using (new LogEntryExit("ConsoleKeyPressReadLoop"))
             {
-                Log("Calling await Console.In.ReadLineAsync()");
-                var line = await Console.In.ReadLineAsync();
-                Log("...after await Console.In.ReadLineAsync() - line.Length: {0}", line.Length);
-
-                if (line.Length == 0)
+                for (; ; )
                 {
-                    Log("Calling observer.OnCompleted() due to empty line");
-                    observer.OnCompleted();
-                    return;
-                }
+                    Log("Calling await Console.In.ReadLineAsync()");
+                    var line = await Console.In.ReadLineAsync();
+                    Log("...after await Console.In.ReadLineAsync() - line.Length: {0}", line.Length);
 
-                foreach (var keyPress in line)
-                {
-                    if (keyPress == 'Q' || keyPress == 'q')
+                    if (line.Length == 0)
                     {
-                        Log("Calling observer.OnCompleted() due to '{0}'", keyPress);
+                        Log("Calling observer.OnCompleted() due to empty line");
                         observer.OnCompleted();
                         return;
                     }
 
-                    Log("Calling observer.OnNext('{0}')", keyPress);
-                    observer.OnNext(Convert.ToString(keyPress));
+                    foreach (var keyPress in line)
+                    {
+                        if (keyPress == 'Q' || keyPress == 'q')
+                        {
+                            Log("Calling observer.OnCompleted() due to '{0}'", keyPress);
+                            observer.OnCompleted();
+                            return;
+                        }
+
+                        Log("Calling observer.OnNext('{0}')", keyPress);
+                        observer.OnNext(Convert.ToString(keyPress));
+                    }
                 }
             }
         }
@@ -89,6 +105,22 @@ namespace App
         private static void Log(string format, params object[] args)
         {
             Console.WriteLine(format, args);
+        }
+
+        private class LogEntryExit : IDisposable
+        {
+            private readonly string _name;
+
+            public LogEntryExit(string name)
+            {
+                _name = name;
+                Log("Entering {0}", _name);
+            }
+
+            public void Dispose()
+            {
+                Log("Leaving {0}", _name);
+            }
         }
     }
 }
